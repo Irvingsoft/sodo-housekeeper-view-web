0e77baec038e899a8933a9e81db555eafc3a8cef
TimeChaser <1872101334@qq.com>
2021-09-06 20:28:56 +0800

    Docker CI/CD

--- setup.sh
+++ setup.sh
@@ -10,4 +10,4 @@ docker run -p 1888:80 -d --name housekeeper housekeeper:$image_version
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 docker logs housekeeper
 #删除build过程中产生的镜像    #docker image prune -a -f
-docker rmi $(docker images -f "dangling=true" -q)
+#docker rmi $(docker images -f "dangling=true" -q)
